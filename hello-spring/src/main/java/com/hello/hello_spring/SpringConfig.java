725bdcbbff410380872bb3ed198fe1daae534662
Duhikim <macksa29@hotmail.com>
2024-12-19 18:02:03 +0900

    [2024.12.9] Add H2 database

--- hello-spring/src/main/java/com/hello/hello_spring/SpringConfig.java
+++ hello-spring/src/main/java/com/hello/hello_spring/SpringConfig.java
@@ -1,13 +1,27 @@
 package com.hello.hello_spring;
 
+import com.hello.hello_spring.repository.JdbcMemberRepository;
+import com.hello.hello_spring.repository.JdbcTemplateMemberRepository;
 import com.hello.hello_spring.repository.MemberRepository;
 import com.hello.hello_spring.repository.MemoryMemberRepository;
 import com.hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
+@EnableTransactionManagement
 public class SpringConfig {
+	private DataSource dataSource;
+
+	@Autowired
+	public SpringConfig(DataSource dataSource){
+		this.dataSource = dataSource;
+	}
+
 	@Bean
 	public MemberService memberService(){
 		return new MemberService(memberRepository());
@@ -15,6 +29,9 @@ public class SpringConfig {
 
 	@Bean
 	public MemberRepository memberRepository(){
-		return new MemoryMemberRepository();
+
+		//return new MemoryMemberRepository();
+		return new JdbcMemberRepository(dataSource);
+		//return new JdbcTemplateMemberRepository(dataSource);
 	}
 }
