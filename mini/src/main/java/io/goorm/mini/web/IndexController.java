c4534b7146fba165a38e34b57913f33d00997b47
Duhikim <macksa29@hotmail.com>
2025-01-24 21:06:12 +0900

    [2025.1.24] Make front page with v0.dev AI tool

---
+++ mini/src/main/java/io/goorm/mini/web/IndexController.java
@@ -0,0 +1,17 @@
+package io.goorm.mini.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+	@GetMapping("/")
+	public String index(Model model) throws Exception {
+
+		// model.addAttribute("resultList", sampleService.selectSampleList(sampleV0))
+
+		return "index";
+	}
+
+}
