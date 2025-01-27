fbea0a02ab53cc893b1859cf83b20fc11a6e04d2
Duhikim <macksa29@hotmail.com>
2025-01-27 15:52:00 +0900

    [2025.1.27] Add Rest Controller

--- mybatis/src/main/java/io/goorm/mybatis/api/board/controller/BoardRestController.java
+++ mybatis/src/main/java/io/goorm/mybatis/api/board/controller/BoardRestController.java
@@ -3,9 +3,8 @@ package io.goorm.mybatis.api.board.controller;
 import io.goorm.mybatis.api.board.service.BoardService;
 import io.goorm.mybatis.board.model.Board;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,8 +13,10 @@ import java.util.List;
 @RequestMapping("/api")
 public class BoardRestController {
 
+	@Autowired
 	private BoardService boardService;
 
+	@GetMapping("")
 	public String index() {
 
 		return "api index";
@@ -32,5 +33,37 @@ public class BoardRestController {
 
 	}
 
+	@GetMapping("/boards/{seq}")
+	public Board get(@PathVariable Long seq) {
+		log.debug("seq : " +seq);
 
+		return boardService.getBoardById(seq);
+	}
+
+	@PostMapping("/boards")
+	public int post(@RequestBody Board board) {
+		log.debug("title : " + board.getTitle());
+		log.debug("contents : " + board.getContents());
+		log.debug("reg_user : " + board.getRegUser());
+
+		return boardService.createBoard(board);
+	}
+
+	@PutMapping("/boards")
+	public Board put(@RequestBody Board board) {
+
+		log.debug("seq : " + board.getSeq());
+		log.debug("title : " + board.getTitle());
+		log.debug("contents : " + board.getContents());
+		log.debug("reg_user : " + board.getRegUser());
+
+		return boardService.updateBoard(board);
+	}
+
+	@DeleteMapping("/boards/{seq}")
+	public boolean delete(@PathVariable Long seq) {
+		log.debug("seq : " + seq());
+
+		return boardService.deleteBoard(seq);
+	}
 }
